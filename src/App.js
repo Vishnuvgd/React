d73074c738f9b9085fbf29600c871f688accbaec
vishnuvgd <vishnudaskallingal7@gmail.com>
2024-05-05 11:35:16 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
-import Todo from "./component/Todo";
-/*import Newone from "./component/Newone";
+/*import Todo from "./component/Todo";
+import Newone from "./component/Newone";
 import Forms from "./component/Forms";*/
 import List from "./component/List";
 class App extends Component {
@@ -17,12 +17,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>{this.state.mystring}</h1>
+        {/*<h1>{this.state.mystring}</h1>
         <h1><button onClick={this.handlechange}>change text</button></h1>
         <Todo sendingdata={this.state.mystringone} />
         <Todo sending={this.state.mystring} />
         <hr></hr>
-        {/*<Newone />
+        <Newone />
         <hr></hr>
         <Forms />*/}
         <h2>List component</h2>
--- src/component/List.js
+++ src/component/List.js
@@ -1,32 +1,48 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from "react";
 
-export default class List extends Component {
-    state = {
-        data: [
-            {
-                id: 1,
-                name: "vishnu",
-                age: 22
-            },
-            {
-                id: 2,
-                name: "sumesh",
-                age: 23
-            }
-        ]
-    };
+export default class List extends PureComponent {
+  state = {
+    score:5,
+    data: [
+      {
+        id: 1,
+        name: "vishnu",
+        age: 22,
+      },
+      {
+        id: 2,
+        name: "sumesh",
+        age: 23,
+      },
+    ],
+  };
 
-    render() {
-        return (
-            <div>
-                <ul>
-                    {this.state.data.map((value, index) => (
-                        <li>
-                            {value.id} - {value.name} - {value.age}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        )
-    }
+  incrementScore = ()=>{
+    this.setState({
+        score:10
+    });
+
+  }
+  /*lifecycle method
+  componentDidMount() {
+    alert();
+  }
+  componentWillUnmount() {
+    console.log("exit");
+  }*/
+  render() {
+    console.log(this.state.score);
+    return (
+      <div>
+        <ul>
+          {this.state.data.map((value, index) => (
+            <li>
+              {value.id} - {value.name} - {value.age}
+            </li>
+          ))}
+        </ul>
+        <button onClick={this.incrementScore}>click here</button>
+      </div>
+    );
+  }
 }
